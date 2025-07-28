f8d1affcbd1a7ff375bac269d6cd473b50c9d534
maruyama hiroki <n09093447580@gmail.com>
2025-07-28 13:33:03 +0900

    first commit.

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ from langchain.schema import HumanMessage, SystemMessage
 load_dotenv()
 
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+st.title(OPENAI_API_KEY+"テスト")
 if not OPENAI_API_KEY:
     OPENAI_API_KEY = st.secrets.get("OPENAI_API_KEY")
 # Streamlitアプリのタイトル
